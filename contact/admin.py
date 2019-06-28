cef882e5a01b2faf2a22f1152460996321285095
VirtualRcoder <shubhamsh2820@gmail.com>
2019-06-28 22:10:46 +0530

    Details page modified

--- contact/admin.py
+++ contact/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import Group
 # Register your models here.
 from . models import Contact
 class ContactsAdmin(admin.ModelAdmin):
-	list_display = ('name', 'gender', 'email', 'info', 'phone')
+	list_display = ('id','name', 'gender', 'email', 'info', 'phone')
 	list_editable = ('info',)
 	list_per_page = 10
 	search_field = ('name', 'gender', 'email', 'info', 'phone')
--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Contact
 # Create your views here.
 def home(request):
-	return render(request, 'index.html')
+	context = {
+		'contacts': Contact.objects.all()
+	}
+	return render(request, 'index.html', context)
+
+def detail(request, id):
+	context = {
+		'contact': get_object_or_404(Contact, pk=id)
+	}
+	return render(request,'detail.html',context)
